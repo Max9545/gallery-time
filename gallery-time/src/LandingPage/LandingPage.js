40874e1811e9decaed4e3a4a9f6f5f2c095eb93d
Max Bregman <bregmanmax91@gmail.com>
2021-04-12 19:57:06 -0600

    Fill out page with basic style

--- gallery-time/src/LandingPage/LandingPage.js
+++ gallery-time/src/LandingPage/LandingPage.js
@@ -34,7 +34,7 @@ function LandingPage ({ geoLocation }) {
 
   return (
     <article className='landing' data-cy="landing">
-      {city && <p className='city-name'data-cy="city-name">{city.name}</p>}
+      {city && <p className='city-name'data-cy="city-name">You are currently in {city.name}, time for culture!</p>}
       {photo && <img className='city-img'data-cy="city-img"src={photo}/>}
       {city && <Link data-cy="to-galleries"className='to-galleries' to={`/city/${city.name}`}>See Galleries</Link>}
     </article>
