f0e138323200f500a7d5b5483334c65606418bed
Max Bregman <bregmanmax91@gmail.com>
2021-04-12 18:29:01 -0600

    Make logog and header a link to the landing page

--- gallery-time/src/Header/Header.js
+++ gallery-time/src/Header/Header.js
@@ -1,10 +1,12 @@
 import './Header.css'
+import { Link } from 'react-router-dom'
 
 function Header () {
   return (
-    <>
-    <h1>GalleryTime</h1>
-    </>
+    <Link to='/'>
+      <h1>GalleryTime</h1>
+      <img src='https://static.thenounproject.com/png/2381701-200.png'/> 
+    </Link>
   )
 }
 export default Header
