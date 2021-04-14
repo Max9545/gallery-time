c381f73a8fa298955b4ae909c72682d2d48b5a84
Max Bregman <67295227+Max9545@users.noreply.github.com>
2021-04-14 11:59:10 -0600

    Make geolocate api call, city search api call and photo api call work with .env file

--- gallery-time/src/apiCalls.js
+++ gallery-time/src/apiCalls.js
@@ -1,11 +1,9 @@
 
-require('dotenv')
-
 const geoLocateUrl = 'https://www.googleapis.com/geolocation/v1/geolocate?key='
-const key = process.env.API_KEY
+const key = process.env.REACT_APP_API_KEY
 
 const mapsUrl = 'https://maps.googleapis.com/maps/api/'
-
+console.log(typeof key)
 export const geoLocatePost = () => {
   return fetch(`${geoLocateUrl}${key}`, {
     method: 'POST',
