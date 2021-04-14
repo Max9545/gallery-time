4ac83b9f701f23e958033e9c9dde72e721f80562
Aaron Foucheaux <afoucheaux@gmail.com>
2021-04-14 09:22:24 -0500

    Added DOM elements to detail view.

--- gallery-time/src/GalleryDetail/GalleryDetail.js
+++ gallery-time/src/GalleryDetail/GalleryDetail.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { detailsSearch } from '../apiCalls.js';
 import { rockyMountainCollegeOfArt } from '../MockData/MockData.js';
 import Header from '../Header/Header.js';
+import { Link } from 'react-router-dom';
 
 function GalleryDetail({ id }) {
   const [detail, setDetail] = useState();
@@ -24,10 +25,29 @@ function GalleryDetail({ id }) {
     <section data-cy='detail-gallery'>
       <Header />
       <h1>{detail && detail.result.name}</h1>
+      <p>Phone Number</p>
+      <p>{detail && detail.result.international_phone_number}</p>
       <article>
+        <h2>Hours</h2>
+        <p>{detail && detail.result.opening_hours.weekday_text[0]}</p>
+        <p>{detail && detail.result.opening_hours.weekday_text[1]}</p>
+        <p>{detail && detail.result.opening_hours.weekday_text[2]}</p>
+        <p>{detail && detail.result.opening_hours.weekday_text[3]}</p>
+        <p>{detail && detail.result.opening_hours.weekday_text[4]}</p>
+        <p>{detail && detail.result.opening_hours.weekday_text[5]}</p>
+        <p>{detail && detail.result.opening_hours.weekday_text[6]}</p>
       </article>
+      <article>
+        <p>{detail && `Average rating ${detail.result.rating} from ${detail.result.user_ratings_total} people.`}</p>
+      </article>
+      <article>
+        <p>Link to Google map.</p>
+        <Link>{detail && detail.result.url}</Link>
+      </article>
+      <button>Favorite</button>
     </section>
   )
+
 }
 
 export default GalleryDetail;
