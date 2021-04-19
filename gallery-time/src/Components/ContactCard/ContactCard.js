d3d5a9dc68c027c805df2f4331096d14141b5ba1
Aaron Foucheaux <afoucheaux@gmail.com>
2021-04-19 05:45:26 -0500

    Basic contact card set up

---
+++ gallery-time/src/Components/ContactCard/ContactCard.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./ContactCard.css";
+import aaron from "../aaron.jpg";
+
+const ContactCard = () => {
+  return (
+    <section className="contact-card">
+      <article className="profile">
+        <img className="profile-pic" src={aaron} alt=" of M.B." />
+        <h3>Max Bregman</h3>
+        <a className="pro-card" href="https://www.linkedin.com/in/max-bregman-216063203/">Max-Linkedin</a>
+        <a className="pro-card" href="https://github.com/Max9545">Max-GitHub</a>
+      </article>
+      <article className="profile">
+        <img className="profile-pic"src={aaron} alt="of A.F." />
+        <h3>Aaron Foucheaux</h3>
+        <a className="pro-card" href="https://www.linkedin.com/in/aaron-foucheaux-891626207/">Aaron-Linkedin</a>
+        <a className="pro-card" href="https://github.com/Afoucheaux">Aaron-GitHub</a>
+      </article>
+    </section>
+  )
+}
+
+export default ContactCard;
