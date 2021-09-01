02013554f644f89bf1bf5e8e7b3074af2862ba22
Max Bregman <67295227+Max9545@users.noreply.github.com>
2021-09-01 14:37:24 -0600

    Style permissions page

--- src/Components/Permission/Permission.js
+++ src/Components/Permission/Permission.js
@@ -23,13 +23,22 @@ function Permission({ setUserCity, setPermission }) {
     }
 
   return(
-    <article>
-      <h3>Allow us use your device's location to locate nearby galleries?</h3>
-      <Link to='/home' onClick={() => handlePermission()}>Yes</Link>
-      <h3>No thank you I will type in the city of my choice</h3>
-      <input type='text' value={cityToSearch} onChange={e => handleChange(e)}/>
-      <input type='submit' value='Find Galleries In...' onClick={() => handleSubmit()}/>
-    </article>
+    <div className='permission-layout'>
+      <div className='logo-card'>
+        <img className='permission-logo' alt="logo" src='https://static.thenounproject.com/png/2381701-200.png'/>
+      </div>
+      <article className='permission-card'>
+        <div>
+          <h3>Allow us use your device's location to locate nearby galleries?</h3>
+          <Link to='/home' className='yes-to-permission'onClick={() => handlePermission()}>Yes</Link>
+        </div>
+        <div>
+          <h3>No thank you I will type in the city of my choice:</h3>
+          <input type='text' className='permission-text'value={cityToSearch} onChange={e => handleChange(e)}/>
+          <input type='submit' className='no-to-permission' value='Find Galleries In...' onClick={() => handleSubmit()}/>
+        </div>
+      </article>
+    </div>
   )
 }
 
